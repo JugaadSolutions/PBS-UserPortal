63fadef862f4e21136d35f5dca2503f7318ed24a
shivakumar <shiv.mytrintrin@gmail.com>
2017-03-03 10:50:44 +0530

    Corrected password policy issue in reset password by Shivu

--- app/passwordstrength.js
+++ app/passwordstrength.js
@@ -8,94 +8,115 @@ $(document).ready(function()
      so everytime user type code will execute
      */
 
-
-
     $('#password').keyup(function()
     {
-        $('#result').html(checkStrength($('#password').val()))
+
+        $('#confirmPassword').val('');
+        $('#result').html(checkStrength($('#password').val()));
+        if(checkStrength($('#password').val()) == "Strong"  && confirmpassword($('#confirmPassword').val())== '')
+        {
+
+            $("#btnSubmit").prop("disabled",false);
+        }
+        else
+        {
+
+            $("#btnSubmit").prop("disabled",true);
+        }
     })
 
     $('#confirmPassword').keyup(function()
     {
-        $('#confirmPasswordresult').html(confirmpassword($('#confirmPassword').val()))
+
+        $('#confirmPasswordresult').html(confirmpassword($('#confirmPassword').val()));
+
+        if(checkStrength($('#password').val()) == "Strong"  && confirmpassword($('#confirmPassword').val())== '')
+        {
+
+            $("#btnSubmit").prop("disabled",false);
+        }
+        else
+        {
+
+            $("#btnSubmit").prop("disabled",true);
+        }
     })
 
 
 
-    /*
-     checkStrength is function which will do the
-     main password strength checking for us
-     */
 
-
-    function confirmpassword(confirmPassword)
-    {
-        var btn = $('#btnSubmit');
-
-        var password = $('#password').val();
-        if(password == confirmPassword)
-        {
-            return ''
-            /*btn.disable=false;*/
-            document.getElementById('btnSubmit')
-            $("#btnSubmit").removeAttr("disabled");
-
-        }
-        else
-            {
-            return 'Password Missmatch'
-        }
-    }
-
-    function checkStrength(password)
-    {
-        //initial strength
-        var strength = 0
-
-        //if the password length is less than 6, return message.
-        if (password.length < 6) {
-            $('#result').removeClass()
-            $('#result').addClass('short')
-            return 'Too short'
-        }
-
-        //length is ok, lets continue.
-
-        //if length is 8 characters or more, increase strength value
-        if (password.length > 7) strength += 1
-
-        //if password contains both lower and uppercase characters, increase strength value
-        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1
-
-        //if it has numbers and characters, increase strength value
-        if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1
-
-        //if it has one special character, increase strength value
-        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1
-
-        //if it has two special characters, increase strength value
-        if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
-
-        //now we have calculated strength value, we can return messages
-
-        //if value is less than 2
-        if (strength < 2 )
-        {
-            $('#result').removeClass()
-            $('#result').addClass('weak')
-            return 'Weak'
-        }
-        else if (strength == 2 )
-        {
-            $('#result').removeClass()
-            $('#result').addClass('good')
-            return 'Good'
-        }
-        else
-        {
-            $('#result').removeClass()
-            $('#result').addClass('strong')
-            return 'Strong'
-        }
-    }
 });
+
+function confirmpassword(confirmPassword)
+{
+    var btn = $('#btnSubmit');
+
+    var password = $('#password').val();
+    var passwordStrength = checkStrength($('#password').val());
+    if((password == confirmPassword) )
+    {
+
+        return '';
+    }
+    else
+    {
+        return 'Password Missmatch'
+    }
+}
+
+/*
+ checkStrength is function which will do the
+ main password strength checking for us
+ */
+
+function checkStrength(password)
+{
+    //initial strength
+    var strength = 0;
+
+    //if the password length is less than 6, return message.
+    if (password.length < 6) {
+        $('#result').removeClass();
+        $('#result').addClass('short');
+        return 'Too short';
+    }
+
+    //length is ok, lets continue.
+
+    //if length is 8 characters or more, increase strength value
+    if (password.length > 7) strength += 1;
+
+    //if password contains both lower and uppercase characters, increase strength value
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1;
+
+    //if it has numbers and characters, increase strength value
+    if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1;
+
+    //if it has one special character, increase strength value
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1;
+
+    //if it has two special characters, increase strength value
+    if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1;
+
+    //now we have calculated strength value, we can return messages
+
+    //if value is less than 2
+    if (strength < 2 )
+    {
+        $('#result').removeClass();
+        $('#result').addClass('weak');
+        return 'Weak';
+    }
+    else if (strength == 2 )
+    {
+        $('#result').removeClass();
+        $('#result').addClass('good');
+        return 'Good';
+    }
+    else
+    {
+        $('#result').removeClass();
+        $('#result').addClass('strong');
+        return 'Strong';
+    }
+}
